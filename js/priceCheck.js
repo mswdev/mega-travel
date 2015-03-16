3186c5678d244769a808d657810390b1b33f3a45
somethingcool56 <noahtarr1@gmail.com>
2015-03-15 22:14:12 -0700

    Added some stuff and Changed some stuff
    Added
    PriceCheck,js
    PriceCheck,.css

---
+++ js/priceCheck.js
@@ -0,0 +1,5 @@
+function vanish(elementName) {
+    var el = document.getElementById(elementName);
+    if (el.style.display == "block") el.style.display = "none";
+    else el.style.display = "block";
+}
